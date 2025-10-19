841cdbf306a30df0ad7f23164dba4a4f7bd226a4
sushain2002 <sushainverma10@gmail.com>
2025-10-19 17:18:58 +0530

    Add Footer component with copyright notice

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-gray-900 text-white text-center py-6 mt-10">
+    <p>© 2025 Your Name. All Rights Reserved.</p>
+  </footer>
+);
+
+export default Footer;
